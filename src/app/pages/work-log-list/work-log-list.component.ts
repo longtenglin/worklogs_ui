edc85ef80e5fb0b41ef82d7dfe135a2447471d57
longtenglin <nanzhaolove@gmail.com>
2025-06-04 10:51:29 +0900

    animation

--- src/app/pages/work-log-list/work-log-list.component.ts
+++ src/app/pages/work-log-list/work-log-list.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {WorkLogItemComponent} from './components/work-log-item/work-log-item.component';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-work-log-list',
   imports: [
     WorkLogItemComponent,
+    NgForOf,
   ],
   templateUrl: './work-log-list.component.html',
   standalone: true,
@@ -12,7 +14,12 @@ import {WorkLogItemComponent} from './components/work-log-item/work-log-item.com
 })
 export class WorkLogListComponent {
 
+  _worklogs = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9
+  ]
+
   onSelectionChange(event: any) {
     console.log(event);
   }
 }
+
