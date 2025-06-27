a73c243698f0bd45587961f71ff933c1011ed356
longtenglin <nanzhaolove@gmail.com>
2025-06-27 18:36:36 +0900

    delete NgIf NgFor

--- src/app/pages/add-work-log/add-work-log.ts
+++ src/app/pages/add-work-log/add-work-log.ts
@@ -1,18 +1,19 @@
 import {Component, ViewEncapsulation} from '@angular/core';
 import {MatFormField, MatOption, MatSelect} from '@angular/material/select';
 import {FormControl, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
-import {LogType, LtlLogTypeComponent} from '../../common/ltl-log-type/ltl-log-type.component';
+import {LogType} from '../../common/ltl-log-type/ltl-log-type.component';
 import {MatError, MatInput, MatLabel} from '@angular/material/input';
-import {QuillEditorComponent, QuillViewHTMLComponent} from 'ngx-quill';
+import {QuillEditorComponent} from 'ngx-quill';
 import {MatChip} from '@angular/material/chips';
 import {MatButton} from '@angular/material/button';
 
 type LogTypeOption = {
-  name: string;
-  value: LogType;
+    name: string;
+    value: LogType;
 }
+
 @Component({
-  selector: 'app-add-work-log',
+    selector: 'app-add-work-log',
     imports: [
         MatSelect,
         MatFormField,
@@ -26,51 +27,51 @@ type LogTypeOption = {
         MatChip,
         MatButton,
     ],
-  standalone: true,
-  templateUrl: './add-work-log.html',
-  styleUrl: './add-work-log.scss',
+    standalone: true,
+    templateUrl: './add-work-log.html',
+    styleUrl: './add-work-log.scss',
     encapsulation: ViewEncapsulation.None
 })
 export class AddWorkLogComponent {
 
-  logTypeControl = new FormControl<LogType | null>(null, Validators.required);
-  logTitleControl = new FormControl<String | null>(null, Validators.required);
+    logTypeControl = new FormControl<LogType | null>(null, Validators.required);
+    logTitleControl = new FormControl<String | null>(null, Validators.required);
 
-  editorContent = "";
+    editorContent: string = "";
 
-  isEditor: boolean = true;
+    isEditor: boolean = true;
 
-  logTypeOptionArr: LogTypeOption[] = [
-    {
-      name: "BUG",
-      value: LogType.BUG
-    },
-    {
-      name: "QA"
-      , value: LogType.QA
-    },
-    {
-      name: "TASK"
-      , value: LogType.TASK
-    }
-  ];
+    logTypeOptionArr: LogTypeOption[] = [
+        {
+            name: "BUG",
+            value: LogType.BUG
+        },
+        {
+            name: "QA"
+            , value: LogType.QA
+        },
+        {
+            name: "TASK"
+            , value: LogType.TASK
+        }
+    ];
 
-  quillModules = {
-    toolbar: [
-      ['bold', 'italic', 'underline', 'strike'],        // 字体样式
-      ['blockquote', 'code-block'],                     // 代码块
-      [{ 'header': 1 }, { 'header': 2 }],               // 自定义按钮值
-      [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-      [{ 'script': 'sub'}, { 'script': 'super' }],      // 下标/上标
-      [{ 'indent': '-1'}, { 'indent': '+1' }],          // 减缩/增加缩进
-      [{ 'direction': 'rtl' }],                         // 文本方向
-      [{ 'size': ['small', false, 'large', 'huge'] }],  // 自定义下拉菜单
-      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-      [{ 'color': [] }, { 'background': [] }],          // 主题默认值的下拉菜单
-      [{ 'font': [] }],
-      [{ 'align': [] }],
-      ['clean'],                                         // 清除格式按钮
-      ['link', 'image', 'video']                         // 链接、图片和视频
-    ]
-  };
+    quillModules = {
+        toolbar: [
+            ['bold', 'italic', 'underline', 'strike'],        // 字体样式
+            ['blockquote', 'code-block'],                     // 代码块
+            [{'header': 1}, {'header': 2}],               // 自定义按钮值
+            [{'list': 'ordered'}, {'list': 'bullet'}],
+            [{'script': 'sub'}, {'script': 'super'}],      // 下标/上标
+            [{'indent': '-1'}, {'indent': '+1'}],          // 减缩/增加缩进
+            [{'direction': 'rtl'}],                         // 文本方向
+            [{'size': ['small', false, 'large', 'huge']}],  // 自定义下拉菜单
+            [{'header': [1, 2, 3, 4, 5, 6, false]}],
+            [{'color': []}, {'background': []}],          // 主题默认值的下拉菜单
+            [{'font': []}],
+            [{'align': []}],
+            ['clean'],                                         // 清除格式按钮
+            ['link', 'image', 'video']                         // 链接、图片和视频
+        ]
+    };
 }
--- src/app/pages/work-log-list/components/log-item-header/log-item-header.component.ts
+++ src/app/pages/work-log-list/components/log-item-header/log-item-header.component.ts
@@ -1,14 +1,12 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {LogType, LtlLogTypeComponent} from '../../../../common/ltl-log-type/ltl-log-type.component';
 import {MatIcon} from '@angular/material/icon';
-import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-log-item-header',
   imports: [
     LtlLogTypeComponent,
     MatIcon,
-    NgIf,
   ],
   templateUrl: './log-item-header.component.html',
   standalone: true,
